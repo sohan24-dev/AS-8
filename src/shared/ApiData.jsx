8996779a6d3db76eac0328de35137bd1f4a8d91c
sohan24-dev <sohan.explorer@gmail.com>
2026-04-29 22:05:26 +0600

    add cache

--- src/shared/ApiData.jsx
+++ src/shared/ApiData.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export const ApiData = async() => {
-     
-   const res = await fetch('http://localhost:3000/data.json')
-    const cowData =await res.json()
+export const ApiData = async () => {
+
+    const res = await fetch("http://localhost:3000/data.json", {
+        cache: "no-store",
+    });
+    const cowData = await res.json()
     return cowData
 };
