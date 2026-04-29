8b91a8e1eb46266f685cbcc9ddde21d88f79803f
sohan24-dev <sohan.explorer@gmail.com>
2026-04-29 22:22:02 +0600

    add api

--- src/shared/ApiData.jsx
+++ src/shared/ApiData.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ApiData = async () => {
 
-    const res = await fetch("http://localhost:3000/data.json", {
+    const res = await fetch("https://ass-08.vercel.app/data.json", {
         cache: "no-store",
     });
     const cowData = await res.json()
